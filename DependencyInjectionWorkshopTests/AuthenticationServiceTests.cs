f98dd1b74a2422c1ee799f64097bf3548f8a6ece
陳家緯 <esawei5190@gmail.com>
2019-05-04 23:36:12 +0800

    add first test: is_valid

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using DependencyInjectionWorkshop.Models;
+﻿using DependencyInjectionWorkshop.Adapters;
+using DependencyInjectionWorkshop.Models;
+using DependencyInjectionWorkshop.Repositories;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace DependencyInjectionWorkshopTests
@@ -9,7 +12,22 @@ namespace DependencyInjectionWorkshopTests
         [Test]
         public void is_valid()
         {
-            var authenticationService = new AuthenticationService();
+            var profile = Substitute.For<IProfile>();
+            var hash = Substitute.For<IHash>();
+            var otpService = Substitute.For<IOtpService>();
+            var failedCounter = Substitute.For<IFailedCounter>();
+            var notification = Substitute.For<INotification>();
+            var logger = Substitute.For<ILogger>();
+
+            var authenticationService = new AuthenticationService(
+                profile, hash, otpService, failedCounter, notification, logger);
+
+            profile.GetPassword("joey").Returns("my hashed password");
+            hash.GetHash("pw").Returns("my hashed password");
+            otpService.GetCurrentOtp("joey").Returns("123456");
+
+            var isValid = authenticationService.Verify("joey", "pw", "123456");
+            Assert.IsTrue(isValid);
         }
     }
 }
