df1920108e52a62c92b5f06f5ea30a5447cb49ed
陳家緯 <esawei5190@gmail.com>
2019-05-04 09:33:19 +0800

    #1 乾淨的胖子

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,56 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string accountId, string password, string otp)
+        {
+            // Get db password
+            var dbPassword = "";
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                dbPassword = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            // Hash input password
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hashedInputPassword = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                hashedInputPassword.Append(theByte.ToString("x2"));
+            }
+            
+            // Verify password
+            if (dbPassword != hashedInputPassword.ToString())
+                return false;
+
+            // Get server OTP
+            var serverOTP = "";
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                serverOTP = response.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                throw new Exception($"web api error, accountId:{accountId}");
+            }
+
+            // Verify OTP
+            if (otp != serverOTP)
+                return false;
+
+            return true;
+        }
     }
 }
